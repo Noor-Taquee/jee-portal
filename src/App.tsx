5649178417a4087762dd339b6762288ab2f98204
Noor Taquee <noortaquee001@gmail.com>
2026-08-05 19:39:48 +0530

    feat: create initial `App` component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./app.css";
+
+export function App() {
+  return (
+    <div id="app" data-theme="light">
+      <div className="panel-container"></div>
+    </div>
+  );
+}
+
+export default App;
