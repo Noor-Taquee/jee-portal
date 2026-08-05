113d53a429ef2823f8b8c0c754187d402c15ce3b
Noor Taquee <noortaquee001@gmail.com>
2026-08-05 19:50:46 +0530

    feat: add `main.tsx` file

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App";
+
+const root = document.getElementById("root") as HTMLDivElement | null;
+
+if (!root) throw ReferenceError("Root not found!");
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
