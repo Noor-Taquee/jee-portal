36843af4fbbd20c73c06b07bbf8c273b6ed588a5
Noor Taquee <noortaquee001@gmail.com>
2026-08-11 21:45:15 +0530

    feat: add `LoginPage` component (#41)

---
+++ src/pages/login-page/LoginForm/UsernameInput.tsx
@@ -0,0 +1,30 @@
+interface UsernameInputProps {
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function UsernameInput({
+  username,
+  setUsername,
+}: UsernameInputProps) {
+  function onChange(e: React.ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+    setUsername(e.target.value);
+  }
+
+  return (
+    <div
+      id="username"
+      className="input-div"
+    >
+      <i className="ph-fill ph-user leading"></i>
+      <input
+        type="text"
+        name="username"
+        id="username"
+        autoComplete="off"
+        value={username}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
