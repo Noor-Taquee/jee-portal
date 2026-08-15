a117c853173a15a1f680dfcf9097b14e74fe3789
Noor Taquee <noortaquee001@gmail.com>
2026-08-15 21:39:40 +0530

    feat: add `InstituteLogo` component with styles

---
+++ src/components/InstituteLogo/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+export default function InstituteLogo() {
+  return (
+    <div className="institute-logo">
+      <img
+        src="https://cdnbbsr.s3waas.gov.in/s388a839f2f6f1427879fc33ee4acf4f66/uploads/2025/10/202510101452469886.png"
+        alt="Institute Logo"
+      />
+    </div>
+  );
+}
