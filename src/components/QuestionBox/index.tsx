17be376ec087e51bdd3c12fb7234dd296ebdc92b
Noor Taquee <noortaquee001@gmail.com>
2026-08-15 21:43:52 +0530

    feat: add "answered and marked for review" variance to `QuestionBox` (#62)

--- src/components/QuestionBox/index.tsx
+++ src/components/QuestionBox/index.tsx
@@ -2,21 +2,35 @@ import "./style.css";
 
 interface QuestionBoxProps {
   number: number | string;
-  className: "unread" | "answered" | "unanswered" | "marked-for-review";
+  className:
+    | "unread"
+    | "answered"
+    | "unanswered"
+    | "marked-for-review"
+    | "ans-marked-for-review";
   onClick?: () => void;
+  ariaHidden?: true;
 }
 
+/** Box surrounding the question number in `QuestionTable` */
 export default function QuestionBox({
   number,
   className,
   onClick,
+  ariaHidden,
 }: QuestionBoxProps) {
+  number = String(number).length === 1 ? `0${number}` : number;
+
   return (
-    <div
+    <button
       className={`question-box ${className}`}
       onClick={onClick}
+      aria-hidden={ariaHidden ? "true" : "false"}
     >
+      {className === "ans-marked-for-review" && (
+        <i className="ph-bold ph-article"></i>
+      )}
       <p>{number}</p>
-    </div>
+    </button>
   );
 }
