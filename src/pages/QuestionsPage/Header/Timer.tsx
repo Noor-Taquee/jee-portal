f9c83b6a30e39a11b5929456357ab613ae52ee03
Noor Taquee <noortaquee001@gmail.com>
2026-09-01 20:59:03 +0530

    ui: add `Timer` component

---
+++ src/pages/QuestionsPage/Header/Timer.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+interface TimerProps {
+  startTime: Date;
+
+  /** Test Duration in _Milliseconds_ */
+  testDuration: number;
+}
+
+export default function Timer({ startTime, testDuration }: TimerProps) {
+  const [leftTime, setLeftTime] = useState<number>(testDuration);
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      const ellapsedTime = Date.now() - startTime.getTime();
+      const remaining = testDuration - ellapsedTime;
+      if (remaining <= 0) {
+        setLeftTime(0);
+        clearInterval(timerInterval);
+      } else {
+        setLeftTime(remaining);
+      }
+    }, 500);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [startTime, testDuration]);
+
+  const totalSeconds = Math.floor(leftTime / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  const pad = (num: number) => String(num).padStart(2, "0");
+
+  return (
+    <div id="timer">
+      <p className={leftTime < 30000 ? "less" : ""}>
+        <span>{pad(hours)}</span>:<span>{pad(minutes)}</span>:
+        <span>{pad(seconds)}</span>
+      </p>
+    </div>
+  );
+}
