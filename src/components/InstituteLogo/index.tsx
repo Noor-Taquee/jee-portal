edca228a306e4261551aad0e5249d55def48c415
Noor Taquee <noortaquee001@gmail.com>
2026-09-05 14:37:53 +0530

    feat: Add logo in the project folder

--- src/components/InstituteLogo/index.tsx
+++ src/components/InstituteLogo/index.tsx
@@ -4,7 +4,7 @@ export default function InstituteLogo() {
   return (
     <div className="institute-logo">
       <img
-        src="https://cdnbbsr.s3waas.gov.in/s388a839f2f6f1427879fc33ee4acf4f66/uploads/2025/10/202510101452469886.png"
+        src={`${import.meta.env.BASE_URL}images/nta_logo_light.png`}
         alt="Institute Logo"
       />
     </div>
