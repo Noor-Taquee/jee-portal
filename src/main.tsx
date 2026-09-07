544d8b083cbbbbf1eb6e3950311886e82f03a6db
Noor Taquee <noortaquee001@gmail.com>
2026-09-07 16:09:46 +0530

    feat: wrap `App` component with `SettingsProvider` (#113)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
 import "./index.css";
 import "./styles/colors.css";
 
 import "katex/dist/katex.min.css";
 
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+
+import SettingsProvider from "./context/SettingsContext";
 import App from "./App";
 
 const root = document.getElementById("root") as HTMLDivElement | null;
@@ -14,6 +15,8 @@ if (!root) throw ReferenceError("Root not found!");
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <SettingsProvider>
+      <App />
+    </SettingsProvider>
   </StrictMode>
 );
