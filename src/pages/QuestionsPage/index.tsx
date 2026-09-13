b96f9afaad179fe9fb3619809de39e501283d3c9
Noor Taquee <noortaquee001@gmail.com>
2026-09-13 12:23:54 +0530

    feat: add message to show exam has not started

--- src/pages/QuestionsPage/index.tsx
+++ src/pages/QuestionsPage/index.tsx
@@ -9,12 +9,34 @@ import Header from "./Header";
 export default function QuestionsPage() {
   const examSession = useExamSession();
 
-  if (!examSession.startedAt) {
-    changeHash("login");
-  }
-
-  if (!examSession.examData) {
-    return <div>Loading...</div>;
+  if (!examSession.examData || !examSession.startedAt) {
+    return (
+      <div
+        className="app-panel"
+        id="questions-page"
+      >
+        <div className="dialogue-box">
+          <p className="dialogue-header">
+            {examSession.examData
+              ? "Test has not started yet !"
+              : "There is no test scheduled !"}
+          </p>
+          <p className="dialogue-text">
+            {examSession.examData
+              ? "Go to the login page to start the test"
+              : "Go to the login page to schedule a test"}
+          </p>
+          <button
+            onClick={() => {
+              changeHash("login");
+            }}
+            className="action-btn"
+          >
+            <p>Back</p>
+          </button>
+        </div>
+      </div>
+    );
   }
 
   return (
