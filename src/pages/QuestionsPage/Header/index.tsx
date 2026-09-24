b030c9aa1ebf9f57e92d4bc49bb92716d3b051ad
Noor Taquee <noortaquee001@gmail.com>
2026-09-24 23:30:23 +0530

    feat: add `SubmitButton` for test submission (#183)

--- src/pages/QuestionsPage/Header/index.tsx
+++ src/pages/QuestionsPage/Header/index.tsx
@@ -1,26 +1,16 @@
 import "./style.css";
 
-import { changeHash } from "../../../hooks/useHash";
-
 import InstituteLogo from "../../../components/InstituteLogo";
-import ActionBtn from "../../../components/ActionBtn";
 
 import Timer from "./Timer";
+import SubmitButton from "./SubmitButton";
 
 export default function Header() {
   return (
     <div id="question-panel-header">
       <InstituteLogo />
       <Timer />
-      <ActionBtn
-        title="End test and submit response"
-        className="submit-btn"
-        onClick={() => {
-          changeHash("result");
-        }}
-      >
-        <p>Submit</p>
-      </ActionBtn>
+      <SubmitButton />
     </div>
   );
 }
